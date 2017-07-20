1e7588576595e3432f9cdc6959b4ce6009712b01
Drake Lambert <drake.askme2@gmail.com>
2017-07-20 10:17:17 -0500

    Added SocketHandler.cs to echo messages

---
+++ WebsocketTestGame/SocketHandler.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebsocketTestGame
+{
+    public class SocketHandler
+    {
+        private List<WebSocket> _socks;
+
+        private ILogger<SocketHandler> _logger;
+
+        public SocketHandler(ILogger<SocketHandler> logger)
+        {
+            _socks = new List<WebSocket>();
+            _logger = logger;
+        }
+
+        public async Task Handle(HttpContext context, WebSocket sock)
+        {
+            var buffer = new byte[1024 * 4];
+            var result = await sock.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await sock.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                _logger.LogInformation($"New Message: { Encoding.UTF8.GetString(buffer) }");
+
+                result = await sock.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await sock.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+    }
+}
--- WebsocketTestGame/Startup.cs
+++ WebsocketTestGame/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,26 +8,40 @@ namespace WebsocketTestGame
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<SocketHandler>();
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, SocketHandler sockHandler)
         {
-            loggerFactory.AddConsole();
+            loggerFactory.AddConsole(LogLevel.Information);
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            app.UseWebSockets();
+            app.Use(async (context, next) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        var sock = await context.WebSockets.AcceptWebSocketAsync();
+                        await sockHandler.Handle(context, sock);
+                    } else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
             });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
